2bace136879952516e76fb2d33e4a72ff55d04cc
Michael Laffan <Michael.Laffan1@marist.edu>
2020-09-28 15:45:33 -0400

    Initial version of virtualtour.py

---
+++ virtualtour.py
@@ -0,0 +1,18 @@
+# main function
+def main():
+    #Title and Introduction
+    print("Marist Virtual Tour")
+    print("Hello everyone! Welcome to Marist College.")
+    print("I'll be your tour guide as I show you some of our locations here")
+    print("on campus. Enjoy the tour.")
+    input("Press the enter key to begin the tour: ")
+    #Location 1 - Student Center
+    print()
+    print("Murray Student Center")
+    print("Our first location here is the Murray Student Center. This location")
+    print("serves as the center point of the college.")
+    
+
+
+
+main()
