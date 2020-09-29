86da207cf954334376f7446970b17129b7f02e16
Michael Laffan <Michael.Laffan1@marist.edu>
2020-09-29 16:15:51 -0400

    Refactored to use variables instead of literals

--- virtualtour.py
+++ virtualtour.py
@@ -1,109 +1,151 @@
 # main function
 def main():
     #Title and Introduction
-    print("Marist Virtual Tour")
-    print("Hello everyone! Welcome to Marist College.")
-    print("I'll be your tour guide as I show you some of our locations here")
-    print("on campus. Enjoy the tour.")
-    input("Press the enter key to begin the tour: ")
+    i1 = "Marist Virtual Tour" 
+    i2 = "Hello everyone! Welcome to Marist College. I'll be your tour guide as I show you some of our locations here on campus. Enjoy the tour."
+    i3 = "Press the enter key to begin the tour: "
+    print(i1)
+    print(i2)
+    print(input(i3))
+    
     #Location 1 - Student Center
     print()
-    print("Murray Student Center")
-    print("Our first location here is the Murray Student Center. This location")
-    print("serves as the center point of the college. It brings together all")
-    print("the members of Marist College. It provides various student services")
-    print("and serves as a place where students can meet and organize events.")
-    print("Some of these services include dining services, student lounge,")
-    print("the campus post office, and the campus bookstore.")
-    input("Press the enter key to go to the next location: ")
+    s1 = "Murray Student Center"
+    s2 =('''Our first location here is the Murray Student Center. This location   
+serves as the center point of the college. It brings together all
+the members of Marist College. It provides various student services
+and serves as a place where students can meet and organize events.
+Some of these services include dining services, student lounge,
+the campus post office, and the campus bookstore.''')
+    s3 = "Press the enter key to go to the next location: "
+    print(s1)
+    print(s2)
+    print(input(s3))
+    
     #Location 2 - Donnelly Hall
     print()
-    print("Donnelly Hall")
-    print("Our next location here is Donnelly Hall. This location mainly houses")
-    print("our technology services and other offices that are available to")
-    print("our students here on campus. Some of these services include")
-    print("Financial services, the Help Desk, Advising and Academic Services")
-    print("and the Computer Lab.")
-    input("Press the enter key to go to the next location: ")
+    d1 = "Donnelly Hall"
+    d2 = ('''Our next location here is Donnelly Hall. This location mainly houses
+our technology services and other offices that are available to
+our students here on campus. Some of these services include
+Financial services, the Help Desk, Advising and Academic Services
+and the Computer Lab.''')
+    d3 = "Press the enter key to go to the next location: "
+    print(d1)
+    print(d2)
+    print(input(d3))
+    
     #Location 3 - Hancock Center
     print()
-    print("Hancock Center")
-    print("Our next location here is the Hancock Center. This location is")
-    print("for our School of Computer Science and Mathematics. However, it also")
-    print("houses our Study Abroad offices, which manages our programs for students")
-    print("who want to go and study in another country, such as our campus in")
-    print("Florence, Italy. It also houses our Investment Center and Trading Room")
-    print("and our Center for Teaching Excellence.")
-    input("Press the enter key to go to the next location: ")
+    h1 = "Hancock Center"
+    h2 = ('''Our next location here is the Hancock Center. This location is
+for our School of Computer Science and Mathematics. However, it also
+houses our Study Abroad offices, which manages our programs for students
+who want to go and study in another country, such as our campus in
+Florence, Italy. It also houses our Investment Center and Trading Room
+and our Center for Teaching Excellence.''')
+    h3 = "Press the enter key to go to the next location: "
+    print(h1)
+    print(h2)
+    print(input(h3))
+    
     #Location 4 - McCann Center
     print()
-    print("McCann Center")
-    print("Our next location here is the McCann Center. This location is mostly")
-    print("known to be our sports center here on campus. It houses our Athletic")
-    print("Department, our fitness center, our Center for Sports Medicine, and")
-    print("other sports services. It also houses our Admissions Visitor Center")
-    print("for people who come to visit our campus.")
-    input("Press the enter key to go to the next location: ")
+    m1 = "McCann Center"
+    m2 = ('''Our next location here is the McCann Center. This location is mostly
+known to be our sports center here on campus. It houses our Athletic
+Department, our fitness center, our Center for Sports Medicine, and
+other sports services. It also houses our Admissions Visitor Center
+for people who come to visit our campus.''')
+    m3 = "Press the enter key to go to the next location: "
+    print(m1)
+    print(m2)
+    print(input(m3))
+    
     #Location #5 - Cannavino Library
     print()
-    print("Cannavino Library")
-    print("Our next location here is the Cannavino Library. This location serves")
-    print("as our main library here on campus. It also serves as a place where")
-    print("students can catch up on their work and study without any distractions.")
-    print("It also houses our Center for Career Services, Center for Multicultural")
-    print("Affairs, and our Digital Education services.")
-    input("Press the enter key to go to the next location: ")
+    c1 = "Cannavino Library"
+    c2 = ('''Our next location here is the Cannavino Library. This location serves
+as our main library here on campus. It also serves as a place where
+students can catch up on their work and study without any distractions.
+It also houses our Center for Career Services, Center for Multicultural
+Affairs, and our Digital Education services.''')
+    c3 = "Press the enter key to go to the next location: "
+    print(c1)
+    print(c2)
+    print(input(c3))
+    
     #Location #6 - The Chapel
     print()
-    print("The Chapel")
-    print("Our next location here is the Chapel. The Chapel serves as an")
-    print("Inter-Faith Prayer room, where anyone can come in for quiet")
-    print("prayer from 7 am to 10 pm. It also provides Mass services")
-    print("Monday through Thursday at 12:15 pm and on Sundays at both")
-    print("12 pm and 6:30 pm.")
-    input("Press the enter key to go to the next location: ")
+    ch1 = "The Chapel"
+    ch2 = ('''Our next location here is the Chapel. The Chapel serves as an
+Inter-Faith Prayer room, where anyone can come in for quiet
+prayer from 7 am to 10 pm. It also provides Mass services
+Monday through Thursday at 12:15 pm and on Sundays at both
+12 pm and 6:30 pm.''')
+    ch3 = "Press the enter key to go to the next location: "
+    print(ch1)
+    print(ch2)
+    print(input(ch3))
+    
     #Location #7 - McCormick Hall
     print()
-    print("McCormick Hall")
-    print("Our next location here is McCormick Hall. This location serves as a")
-    print("residence hall for our upperclassmen. Unlike the freshman residence")
-    print("halls we have here on campus, residents of this hall are responsible")
-    print("for their own cooking and cleaning of their apartment. This location")
-    print("also serves as one of our dining locations here on campus, also")
-    print("known as North End Dining.")
-    input("Press the enter key to go to the next location: ")
+    mc1 = "McCormick Hall"
+    mc2 = ('''Our next location here is McCormick Hall. This location serves as a
+residence hall for our upperclassmen. Unlike the freshman residence
+halls we have here on campus, residents of this hall are responsible
+for their own cooking and cleaning of their apartment. This location
+also serves as one of our dining locations here on campus, also
+known as North End Dining.''')
+    mc3 = "Press the enter key to go to the next location: "
+    print(mc1)
+    print(mc2)
+    print(input(mc3))
+    
     #Location 8 - Steel Plant Studios and Gallery
     print()
-    print("Steel Plant Studios and Gallery")
-    print("Our next location is the Steel Plant Studios and Gallery, commonly")
-    print("known as the Steel Plant. This building mainly houses our fashion")
-    print("and design programs that we have available here on campus. It also")
-    print("houses our Art Gallery and our Digital Media and Fine Arts programs")
-    print("here as well.")
-    input("Press the enter key to go to the next location: ")
+    sp1 = "Steel Plant Studios and Gallery"
+    sp2 = ('''Our next location is the Steel Plant Studios and Gallery, commonly
+known as the Steel Plant. This building mainly houses our fashion
+and design programs that we have available here on campus. It also
+houses our Art Gallery and our Digital Media and Fine Arts programs
+here as well.''')
+    sp3 = "Press the enter key to go to the next location: "
+    print(sp1)
+    print(sp2)
+    print(input(sp3))
+    
     #Location 9 - Science and Allied Health Building
     print()
-    print("Science and Allied Health Building")
-    print("Our next location here is the Science and Allied Health Building.")
-    print("This building mainly houses our science and medical field programs.")
-    print("It also houses our School of Science and our labratories such as the")
-    print("Cognitive Computing Lab and Gross Anatomy Lab. Anyone interested in")
-    print("the medical field will likely take most of their classes here.")
-    input("Press the enter key to go to the last location: ")
+    sc1 = "Science and Allied Health Building"
+    sc2 = ('''Our next location here is the Science and Allied Health Building.
+This building mainly houses our science and medical field programs.
+It also houses our School of Science and our labratories such as the
+Cognitive Computing Lab and Gross Anatomy Lab. Anyone interested in
+the medical field will likely take most of their classes here.''')
+    sc3 = "Press the enter key to go to the last location: "
+    print(sc1)
+    print(sc2)
+    print(input(sc3))
+    
     #Location 10 - Dyson Center
     print()
-    print("Dyson Center")
-    print("Our last location that I will be showing you here at Marist College")
-    print("is the Dyson Center. This building houses our School of Management")
-    print("and School of Social and Behavioral Sciences. It also houses our")
-    print("Teacher Education Program for any students that wish to pursue a")
-    print("career in teaching and it also has a coffee shop.")
-    input("Press the enter key to end the tour: ")
+    dy1 = "Dyson Center"
+    dy2 = ('''Our last location that I will be showing you here at Marist College
+is the Dyson Center. This building houses our School of Management
+and School of Social and Behavioral Sciences. It also houses our
+Teacher Education Program for any students that wish to pursue a
+career in teaching and it also has a coffee shop.''')
+    dy3 = "Press the enter key to end the tour: "
+    print(dy1)
+    print(dy2)
+    print(input(dy3))
+    
     #Closing
     print()
-    print("Thank you everyone for visiting Marist College. I hope you enjoyed")
-    print("the tour and consider applying to Marist College. Goodbye!")
-    
+    cl = ('''Thank you everyone for visiting Marist College. I hope you enjoyed
+the tour and consider applying to Marist College. Goodbye!''')
+    print(cl)
 
 
 main()
