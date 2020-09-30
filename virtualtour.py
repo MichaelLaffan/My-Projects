75cbeb5090f2c0cc1ed35c1e3451bc0c99cbb589
Michael Laffan <Michael.Laffan1@marist.edu>
2020-09-30 16:47:05 -0400

    Provided more detail about each location

--- virtualtour.py
+++ virtualtour.py
@@ -18,10 +18,17 @@ the members of Marist College. It provides various student services
 and serves as a place where students can meet and organize events.
 Some of these services include dining services, student lounge,
 the campus post office, and the campus bookstore.''')
-    s3 = "Press the enter key to go to the next location: "
+    s3 = "Press the enter key for information on the dorms next to the Student Center: "
+    s4 = ('''The dorms that are adjacent to the Student Center are Champangat Hall
+and Midrise Hall. Champagnat is a freshman residence consisting of nine floors and has
+rooms consisting of doubles. Midrise hall is a suite style residence hall and houses
+freshman, sophomore and transfer students.''')
+    s5 = "Press the enter key to go to the next location: "
     print(s1)
     print(s2)
     print(input(s3))
+    print(s4)
+    print(input(s5))
     count = count + 1
     
     #Location 2 - Donnelly Hall
@@ -32,10 +39,16 @@ our technology services and other offices that are available to
 our students here on campus. Some of these services include
 Financial services, the Help Desk, Advising and Academic Services
 and the Computer Lab.''')
-    d3 = "Press the enter key to go to the next location: "
+    d3 = "Press the enter key for a list of the other services: "
+    d4 = ('''The other services we have available here are the Accomodations
+and Accessibility, the Registrar office, Safety and Security, ResNet, and
+Business and Financial Affairs.''')
+    d5 = "Press the enter key to go to the next location: "
     print(d1)
     print(d2)
     print(input(d3))
+    print(d4)
+    print(input(d5))
     count = count + 1
     
     #Location 3 - Hancock Center
@@ -47,10 +60,15 @@ houses our Study Abroad offices, which manages our programs for students
 who want to go and study in another country, such as our campus in
 Florence, Italy. It also houses our Investment Center and Trading Room
 and our Center for Teaching Excellence.''')
-    h3 = "Press the enter key to go to the next location: "
+    h3 = "Press the enter key for a list of some of our Study Abroad locations: "
+    h4 = ('''Our Study Abroad locations include Japan, Italy, Greece, France,
+Australia, Spain, and China.''')
+    h5 = "Press the enter key to go to the next location: "
     print(h1)
     print(h2)
     print(input(h3))
+    print(h4)
+    print(input(h5))
     count = count + 1
     
     #Location 4 - McCann Center
@@ -61,10 +79,15 @@ known to be our sports center here on campus. It houses our Athletic
 Department, our fitness center, our Center for Sports Medicine, and
 other sports services. It also houses our Admissions Visitor Center
 for people who come to visit our campus.''')
-    m3 = "Press the enter key to go to the next location: "
+    m3 = "Press the enter key for a list sports we have here: "
+    m4 = ('''The primary sports that are available here at Marist are
+basketball, soccer, volleyball, flag football, and softball.''')
+    m5 = "Press the enter key to go to the next location: "
     print(m1)
     print(m2)
     print(input(m3))
+    print(m4)
+    print(input(m5))
     count = count + 1
     
     #Location #5 - Cannavino Library
@@ -75,10 +98,17 @@ as our main library here on campus. It also serves as a place where
 students can catch up on their work and study without any distractions.
 It also houses our Center for Career Services, Center for Multicultural
 Affairs, and our Digital Education services.''')
-    c3 = "Press the enter key to go to the next location: "
+    c3 = "Press the enter key for information on the digital library: "
+    c4 = ('''The library has a tool called Fox Hunt that allows you to
+look for any book that is available in the library. It also allows you to
+access any workshop that gives you information about other services we have
+here on campus such as counseling.''')
+    c5 = "Press the enter key to go to the next location: "
     print(c1)
     print(c2)
     print(input(c3))
+    print(c4)
+    print(input(c5))
     count = count + 1
     
     #Location #6 - The Chapel
@@ -89,10 +119,16 @@ Inter-Faith Prayer room, where anyone can come in for quiet
 prayer from 7 am to 10 pm. It also provides Mass services
 Monday through Thursday at 12:15 pm and on Sundays at both
 12 pm and 6:30 pm.''')
-    ch3 = "Press the enter key to go to the next location: "
+    ch3 = "Press the enter key for information on our spirit groups: "
+    ch4 = ('''The Chapel hosts spirit groups for men, women, and different
+faiths. Some of the topics discussed in these spirit groups are personal
+identity, transitions into college, and sprituality.''')
+    ch5 = "Press the enter key to go to the next location: "
     print(ch1)
     print(ch2)
     print(input(ch3))
+    print(ch4)
+    print(input(ch5))
     count = count + 1
     
     #Location #7 - McCormick Hall
@@ -104,10 +140,16 @@ halls we have here on campus, residents of this hall are responsible
 for their own cooking and cleaning of their apartment. This location
 also serves as one of our dining locations here on campus, also
 known as North End Dining.''')
-    mc3 = "Press the enter key to go to the next location: "
+    mc3 = "Press the enter key for a list of our food options: "
+    mc4 = ('''Some of our food options that are available at North End
+Dining are cheese quesadillas, cheeseburgers, crispy chicken sandwich,
+and cheese and pepperoni pizza.''')
+    mc5 = "Press the enter key to go to the next location: "
     print(mc1)
     print(mc2)
     print(input(mc3))
+    print(mc4)
+    print(input(mc5))
     count = count + 1
     
     #Location 8 - Steel Plant Studios and Gallery
@@ -118,10 +160,16 @@ known as the Steel Plant. This building mainly houses our fashion
 and design programs that we have available here on campus. It also
 houses our Art Gallery and our Digital Media and Fine Arts programs
 here as well.''')
-    sp3 = "Press the enter key to go to the next location: "
+    sp3 = "Press the enter key for more information on the Art Gallery: "
+    sp4 = ('''The Art Gallery is a 3200 square foot art exhibition space
+and retains the industrial look of the former steel plant. It focuses on
+contemporary regional artists working in fine arts media.''')
+    sp5 = "Press the enter key to go to the next location: "
     print(sp1)
     print(sp2)
     print(input(sp3))
+    print(sp4)
+    print(input(sp5))
     count = count + 1
     
     #Location 9 - Science and Allied Health Building
@@ -132,10 +180,16 @@ This building mainly houses our science and medical field programs.
 It also houses our School of Science and our labratories such as the
 Cognitive Computing Lab and Gross Anatomy Lab. Anyone interested in
 the medical field will likely take most of their classes here.''')
-    sc3 = "Press the enter key to go to the last location: "
+    sc3 = "Press the enter key for information on our Physician Assist Program: "
+    sc4 = ('''Our Physician Assistant program is a 24 month, full time program
+designed to prepare our students for clinical practice. Students learn how to
+care in a variety of settings such as in the operating and emergency room.''')
+    sc5 = "Press the enter key to go to the last location: "
     print(sc1)
     print(sc2)
     print(input(sc3))
+    print(sc4)
+    print(input(sc5))
     count = count + 1
     
     #Location 10 - Dyson Center
@@ -146,10 +200,16 @@ is the Dyson Center. This building houses our School of Management
 and School of Social and Behavioral Sciences. It also houses our
 Teacher Education Program for any students that wish to pursue a
 career in teaching and it also has a coffee shop.''')
-    dy3 = "Press the enter key to end the tour: "
+    dy3 = "Press the enter key for a list of our teacher education programs: "
+    dy4 = ('''Some of our programs of study for our education majors is the
+Adolescence Education program, Special Education program, and M.A.s in Education
+Psychology and Initial Teaching Certification.''')
+    dy5 = "Press the enter key to end the tour: "
     print(dy1)
     print(dy2)
     print(input(dy3))
+    print(dy4)
+    print(input(dy5))
     count = count + 1
     
     #Closing
